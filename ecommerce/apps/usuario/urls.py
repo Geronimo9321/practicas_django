41ccd58b9db1298198a45429938a41546986a36c
Geronimo9321 <gero00franco01@gmail.com>
2025-07-29 21:00:14 -0300

    solucionar el login

--- ecommerce/apps/usuario/urls.py
+++ ecommerce/apps/usuario/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'usuario'
 
 urlpatterns = [
-    path('Registro/', views.RegistroUsuario.as_views(), name='path_registro'),
     path('Login/', auth.LoginView.as_views(template_name='usuario/login.html'), name='path_login'),
     path('Logout/', auth.LogoutView.as_views(), name='path_logout'),
+    
+    path('Registro/', views.RegistroUsuario.as_views(), name='path_registro'),
 ]
