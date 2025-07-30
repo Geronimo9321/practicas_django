1cb50210465d8bb622e7141cbe3c2fc30ee07925
Geronimo9321 <gero00franco01@gmail.com>
2025-07-30 13:17:55 -0300

    arreglos en login y registro

--- ecommerce/apps/usuario/urls.py
+++ ecommerce/apps/usuario/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'usuario'
 
 urlpatterns = [
-    path('Login/', auth.LoginView.as_views(template_name='usuario/login.html'), name='path_login'),
-    path('Logout/', auth.LogoutView.as_views(), name='path_logout'),
+    path('Login/', auth.LoginView.as_view(template_name='usuario/login.html'), name='path_login'),
+    path('Logout/', auth.LogoutView.as_view(), name='path_logout'),
     
-    path('Registro/', views.RegistroUsuario.as_views(), name='path_registro'),
+    path('Registro/', views.RegistroUsuario.as_view(), name='path_registro'),
 ]
--- ecommerce/apps/usuario/views.py
+++ ecommerce/apps/usuario/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import FormularioRegistroUsuario
 
 # Create your views here.
+class RegistroUsuario(CreateView):
+    template_name = 'usuario/registro.html'
+    form_class = FormularioRegistroUsuario
+    success_url = reverse_lazy('usuario:path_login')
